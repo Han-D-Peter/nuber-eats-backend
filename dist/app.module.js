fa7ad90ed502df46497adcb24606a4b1ece2700e
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2020-12-02 21:21:11 +0900

    1.0

--- dist/app.module.js
+++ dist/app.module.js
@@ -8,15 +8,19 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
-const app_controller_1 = require("./app.controller");
-const app_service_1 = require("./app.service");
+const graphql_1 = require("@nestjs/graphql");
+const path_1 = require("path");
 let AppModule = class AppModule {
 };
 AppModule = __decorate([
     common_1.Module({
-        imports: [],
-        controllers: [app_controller_1.AppController],
-        providers: [app_service_1.AppService],
+        imports: [
+            graphql_1.GraphQLModule.forRoot({
+                autoSchemaFile: path_1.join(process.cwd(), 'src/schema.gql'),
+            }),
+        ],
+        controllers: [],
+        providers: [],
     })
 ], AppModule);
 exports.AppModule = AppModule;
